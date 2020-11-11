febcb0a165cd0708de489d203a19bdc61ca6d287
hhutku <hhutku.wa@gmail.com>
2020-11-11 05:07:15 -0800

    fahreneit celcius function added

--- weather-dashboard.js
+++ weather-dashboard.js
@@ -1,25 +1,29 @@
 var appID = "d0c7a236e73b7d700b29c6edb3005c4a";
 var searchCity = $('#searchCity')
 var searchBtn = $('#searchBtn');
-var city1 = $('#city1');
 var cities = $('.city');
-var resultCity = $('#resultCity');
-var resultTemp = $('.resultTemp');
-var resultHum = $('.resultHum');
-var resultDesc = $('.description');
-var resultWind = $('.resultWind');
-var resultUv = $('.resultUv');
+var resultCityEl = $('#resultCity');
+var resultTempEl = $('.resultTemp');
+var resultHumEl = $('.resultHum');
+var resultDescEl = $('.description');
+var resultWindEl = $('.resultWind');
+var resultUvEl = $('.resultUv');
+var uvColor = $('#resultUv');
 var weatherImageTitleEl = $('#weatherImageTitle');
+var fahreneitEl = $('#fahreneit');
+var celciusEl = $('#celcius');
 var lasCities = [];
 
+
 if (!localStorage.getItem("lastCities")) {
-    var cities = ["SEATTLE","SAN FRANCISCO", "NEW YORK CITY", "LOS ANGELES", "CHICAGO", "HOUSTON", "PHOENIX", "PHILADELPHIA", "SAN DIEGO", "BOSTON"];
+    var cities = ["SEATTLE", "SAN FRANCISCO", "NEW YORK CITY", "LOS ANGELES", "CHICAGO", "HOUSTON", "PHOENIX", "PHILADELPHIA", "SAN DIEGO", "BOSTON"];
     localStorage.setItem("lastCities", JSON.stringify(cities));
 }
+var cityName = JSON.parse(localStorage.getItem("lastCities"))[0];
 
 lastCities = JSON.parse(localStorage.getItem("lastCities"));
-function cityHistory() {
 
+function cityHistoryDisplay() {
     for (i = 0; i < lastCities.length; i++) {
         $("#city" + i).text(lastCities[i]);
     }
@@ -53,29 +57,38 @@ function apiCall(cityName) {
                 localStorage.setItem("lastCities", JSON.stringify(lastCities));
             }
 
-            displayCurrentCity(cityName,response);
+            displayCurrentCity(cityName, response);
             displayUV(response.coord.lat, response.coord.lon, appID);
             displayFiveDays(cityName, appID);
-            cityHistory();
+            cityHistoryDisplay();
 
         }
 
     })
 }
 
-function displayCurrentCity(cityName,response){
+function displayCurrentCity(cityName, response) {
 
-    resultCity.text(cityName.toUpperCase());
+    resultCityEl.text(cityName.toUpperCase());
     var currentDate = moment().format(" (M/D/YYYY) ");
-    var currentTemp = ((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(1);
+
+    var currentFah = ((response.main.temp - 273.15) * 9 / 5 + 32).toFixed(1);
+    var currentCel = (response.main.temp - 273.15).toFixed(1);
+
     var currentHumidity = response.main.humidity;
     var currentWind = ((response.wind.speed) * 2.237).toFixed(1);
     var currentDesc = response.weather[0].description;
     $('#date').text(currentDate);
-    resultTemp.text("Temperature: " + currentTemp + "℉");
-    resultHum.text("Humidity: " + currentHumidity + "%");
-    resultWind.text("Wind Speed: " + currentWind + " MPH");
-    resultDesc.text("Description: " + currentDesc);
+    resultTempEl.text("Temperature:  ");
+
+    fahOrCelcius(currentFah,currentCel);
+
+    fahreneitEl.text(" ℉");
+    $('#slash').text(" / ");
+    celciusEl.text("°C");
+    resultHumEl.text("Humidity: " + currentHumidity + "%");
+    resultWindEl.text("Wind Speed: " + currentWind + " MPH");
+    resultDescEl.text("Description: " + capFirstLetter(currentDesc));
     weatherImageTitleEl.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
 }
@@ -115,28 +128,56 @@ function displayUV(latt, longt, appID) {
     }).then(function (uviJSON) {
         var uvi = JSON.stringify(uviJSON);
         var uviObj = JSON.parse(uvi);
-        resultUv.text("UV Index: ");
-        $('#resultUv').text(uviObj.value)
+        resultUvEl.text("UV Index: ");
+        uvColor.text(uviObj.value)
         var uvValue = parseInt(uviObj.value);
         if (uvValue < 2) {
-            $('#resultUv').css("background-color", "rgb(34, 151, 34)")
+            uvColor.css("background-color", "rgb(34, 151, 34)")
         } else if (uvValue < 6) {
-            $('#resultUv').css("background-color", "yellow")
+            uvColor.css("background-color", "yellow")
         } else if (uvValue < 8) {
-            $('#resultUv').css("background-color", "orange")
+            uvColor.css("background-color", "orange")
         } else if (uvValue < 11) {
-            $('#resultUv').css("background-color", "red")
+            uvColor.css("background-color", "red")
         } else {
-            $('#resultUv').css("background-color", "#6538708f")
+            uvColor.css("background-color", "#6538708f")
         }
     });
 }
-if (!JSON.parse(localStorage.getItem("lastCities"))) {
-    cityName = "Seattle";
-} else {
-    searchedCity = JSON.parse(localStorage.getItem("lastCities"))[0];
-    var cityName = searchedCity;
+function fahOrCelcius(currentFah,currentCel){
+
+    if ($('#fahreneit').hasClass("flag")) {
+        $('.resultTemp1').text(currentFah);
+    } else {
+        $('.resultTemp1').text(currentCel)
+    }
+    fahreneitEl.click(function () {
+     
+            fahreneitEl.addClass("bold");
+            celciusEl.removeClass("bold");
+            $('.resultTemp1').text(currentFah) 
+            $('#fahreneit').addClass("flag")         
+        
+    })
+    celciusEl.click(function () {
+       
+            $(fahreneitEl).removeClass("bold")
+            $(celciusEl).addClass("bold");
+
+            $('#fahreneit').removeClass("flag")  
+            
+            $('.resultTemp1').text(currentCel)
+      
+    })
+
 }
 
-cityHistory();
+
+cityHistoryDisplay();
 apiCall(cityName);
+
+
+
+function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}

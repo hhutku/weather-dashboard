977266f3eb44c802a1a0dd5d5e56828c91ecca12
hhutku <hhutku.wa@gmail.com>
2020-11-14 17:48:11 -0800

    comments added

--- weather-dashboard.js
+++ weather-dashboard.js
@@ -1,4 +1,6 @@
+// API key 
 var appID = "d0c7a236e73b7d700b29c6edb3005c4a";
+// finding and assigning elements
 var searchCity = $('#searchCity')
 var searchBtn = $('#searchBtn');
 var cities = $('.city');
@@ -12,34 +14,38 @@ var uvColor = $('#resultUv');
 var weatherImageTitleEl = $('#weatherImageTitle');
 var fahreneitEl = $('#fahreneit');
 var celciusEl = $('#celcius');
+// creating an array for the cities in local storage
 var lasCities = [];
 
-
+// storing the cities initially from a default array to local storage
 if (!localStorage.getItem("lastCities")) {
     var cities = ["SEATTLE", "SAN FRANCISCO", "NEW YORK CITY", "LOS ANGELES", "CHICAGO", "HOUSTON", "PHOENIX", "PHILADELPHIA", "SAN DIEGO", "BOSTON"];
     localStorage.setItem("lastCities", JSON.stringify(cities));
 }
+// display the first(top) city 
 var cityName = JSON.parse(localStorage.getItem("lastCities"))[0];
-
+// parsing the data from local storage
 lastCities = JSON.parse(localStorage.getItem("lastCities"));
-
+// function for displaying city buttons
 function cityHistoryDisplay() {
     for (i = 0; i < lastCities.length; i++) {
         $("#city" + i).text(lastCities[i]);
     }
 }
-
+// searching cities from search button
 $("#searchBtn").click(function (e) {
     e.preventDefault();
     apiCall(searchCity.val());
     searchCity.val("");
 })
-
+// searching cities from history buttons
 $(".city").click(function (e) {
     e.preventDefault();
     apiCall($(this).text());
 
 })
+
+// function for API call with the parameter city name
 function apiCall(cityName) {
 
     var weather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + appID;
@@ -48,15 +54,15 @@ function apiCall(cityName) {
         url: weather,
         method: "GET"
     }).then(function (response) {
-
+// checking if there is a result from API 
         if (response.name) {
-
+// arranging local storage array with the last search
             if (!lastCities.includes(cityName.toUpperCase())) {
                 lastCities.splice(9, 1);
                 lastCities.splice(0, 0, (cityName).toUpperCase());
                 localStorage.setItem("lastCities", JSON.stringify(lastCities));
             }
-
+// calling the methods to populate the areas 
             displayCurrentCity(cityName, response);
             displayUV(response.coord.lat, response.coord.lon, appID);
             displayFiveDays(cityName, appID);
@@ -66,7 +72,7 @@ function apiCall(cityName) {
 
     })
 }
-
+// function for populating current(today's) result area.
 function displayCurrentCity(cityName, response) {
 
     resultCityEl.text(cityName.toUpperCase());
@@ -81,6 +87,7 @@ function displayCurrentCity(cityName, response) {
     $('#date').text(currentDate);
     resultTempEl.text("Temperature:  ");
 
+// calling the function to toggle with fahreneit and celcius
     fahOrCelcius(currentFah,currentCel);
 
     fahreneitEl.text(" ℉");
@@ -92,7 +99,7 @@ function displayCurrentCity(cityName, response) {
     weatherImageTitleEl.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
 }
-
+// function for displaying five day forecast area
 function displayFiveDays(cityName, appID) {
     var fiveDayForcastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + appID;
 
@@ -105,19 +112,17 @@ function displayFiveDays(cityName, appID) {
         for (var i = 0; i < fiveDays.list.length; i++) {
             var fiveDaysDates = fiveDays.list[i].dt_txt.split(" ")[0];
             var time = fiveDays.list[i].dt_txt.split(" ")[1];
-
             if (time === "15:00:00") {
-
-                $("#day" + count).children(".card-date").html(fiveDaysDates);
+                $("#day" + count).children(".card-date").text(fiveDaysDates);
                 $("#weather_image" + count).attr("src", "https://openweathermap.org/img/w/" + fiveDays.list[i].weather[0].icon + ".png");
-                $("#day" + count).children(".card-temp").html("Temp: " + fiveDays.list[i].main.temp.toFixed(1) + " °F");
-                $("#day" + count).children(".card-humid").html("Humidity: " + fiveDays.list[i].main.humidity + " %");
+                $("#day" + count).children(".card-temp").text("Temp: " + fiveDays.list[i].main.temp.toFixed(1) + " °F");
+                $("#day" + count).children(".card-humid").text("Humidity: " + fiveDays.list[i].main.humidity + " %");
                 count++;
             }
         }
     });
 }
-
+// function to populate uv index 
 function displayUV(latt, longt, appID) {
 
     var uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latt + "&lon=" + longt + "&APPID=" + appID;
@@ -144,6 +149,7 @@ function displayUV(latt, longt, appID) {
         }
     });
 }
+// function  to toggle with fahreneit and celcius
 function fahOrCelcius(currentFah,currentCel){
 
     if ($('#fahreneit').hasClass("flag")) {
@@ -172,12 +178,12 @@ function fahOrCelcius(currentFah,currentCel){
 
 }
 
-
+// initialize the search history buttons and populate the result areas when the page is opened 
 cityHistoryDisplay();
 apiCall(cityName);
 
 
-
+// function for capitilazing the first letter
 function capFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
